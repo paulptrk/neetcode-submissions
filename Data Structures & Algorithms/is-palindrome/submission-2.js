a02e6d5a5383dca18d916e2592d4171aaef11bd7
Paul Patrick <89486089+paulptrk@users.noreply.github.com>
2026-07-20 16:21:42 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            if (!/[a-zA-Z0-9]/.test(s[left])) {
+                left++;
+                continue;
+            } else if (!/[a-zA-Z0-9]/.test(s[right])) {
+                right--;
+                continue;
+            } else if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
