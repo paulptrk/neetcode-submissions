3cca83ea3645a504e9e885b76bbc1f43696885a6
Paul Patrick <89486089+paulptrk@users.noreply.github.com>
2026-09-04 14:52:16 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        const stack = [];
+        for (let i = 0; i < operations.length; i++) {
+            const operation = operations[i];
+            switch (operation) {
+                case "+":
+                    stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+                    break;
+                case "D":
+                    stack.push(stack[stack.length - 1] * 2);
+                    break;
+                case "C":
+                    stack.pop();
+                    break;
+                default:
+                    stack.push(+operation);
+            }
+        }
+
+        return stack.reduce((total, curr) => total + curr, 0);
+    }
+}
